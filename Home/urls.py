6c72e2df98d7a2d3d862365319566796ade79921
MAQ-97 <51742322+MAQ-97@users.noreply.github.com>
2023-01-17 18:13:10 +0500

    Changes

--- Home/urls.py
+++ Home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('',views.index,name='home'),
     path('about',views.about,name='about'),
     path('contact',views.contact,name='contact'),
-    path('contact/submit',views.contact_submit,name='contact_submit')
+    path('contact/view',views.contact_view,name='contact_view'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse 
 from datetime import datetime
 from Home.models import Contact
+from django.contrib import messages
 # Create your views here.
 def index(request):
     Page=request.GET.get('id')
@@ -19,14 +20,22 @@ def contact(request):
     data={
         "page_name":"Contact"
     }
+    if request.method=="POST":
+        name=request.POST.get('name');
+        email=request.POST.get('email');
+        phone=request.POST.get('phone');
+        message=request.POST.get('message');
+        contact =Contact(name=name,email=email,phone=phone,message=message,date=datetime.today())
+        contact.save()
+        messages.success(request, 'Contact Submitted.')
     return render(request,'contact.html',data)
     # return HttpResponse("<h2>This is Contact Us Page</h2>")
 
-def contact_submit(request):
-    name=request.POST.get('name');
-    email=request.POST.get('email');
-    phone=request.POST.get('phone');
-    message=request.POST.get('message');
-    contact =Contact(name=name,email=email,phone=phone,message=message,date=datetime.today())
-    contact.save()
-    # raise Exception(request.POST.get("name"))
+def contact_view(request):
+    contact=Contact.objects.all()
+    data={
+        "page_name":"Contact View",
+        "contacts":contact,
+        "count":0
+    }
+    return render(request,'contact_view.html',data)
